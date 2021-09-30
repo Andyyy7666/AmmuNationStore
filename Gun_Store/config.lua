bc73bcd602d6f1dd9fe58baf9833c031f043944b
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2021-10-01 00:22:07 +0200

    Add files via upload

---
+++ Gun_Store/config.lua
@@ -0,0 +1,19 @@
+------------------------------------------------------------------------
+------------------------------------------------------------------------
+--																	  --
+--     For support join my discord: https://discord.gg/Z9Mxu72zZ6     --
+--																	  --
+------------------------------------------------------------------------
+------------------------------------------------------------------------
+
+Config = {
+	MenuPosition = "right",
+	
+	--Default menu width is 80
+	MenuWidth = 80,
+
+	-- Custom banner https://imgur.com/upload
+	CustomMenuEnabled = true,
+	MenuImage = "https://i.imgur.com/YHgZX1E.png",
+	-- Another cool image: https://i.imgur.com/ON97ple.png
+}
