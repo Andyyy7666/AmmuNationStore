2f7156a85530894e49028663025a614e30882068
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2021-10-17 19:30:29 +0200

    Add files via upload

---
+++ Gun_Store/fxmanifest.lua
@@ -0,0 +1,22 @@
+------------------------------------------------------------------------
+------------------------------------------------------------------------
+--		    Don't touch if you don't know what you're doing.		  --
+--     For support join my discord: https://discord.gg/Z9Mxu72zZ6     --
+--																	  --
+------------------------------------------------------------------------
+------------------------------------------------------------------------
+
+fx_version "cerulean"
+game "gta5"
+
+title "Gun Store"
+description "Gun Store with Andyyy Currency System"
+author "Andyyy#7666"
+version "v1.1"
+
+client_scripts {
+    "source/NativeUI.lua",
+    "config.lua",
+    "source/menu.lua",
+    "source/client.lua",
+}
