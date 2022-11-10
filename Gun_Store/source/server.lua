e4b6b7af7d6e16e97ba8a8d5aa5c8ac4e8692161
Marshular <marshy0909@gmail.com>
2022-11-09 18:59:25 -0800

    Update server.lua

--- Gun_Store/source/server.lua
+++ Gun_Store/source/server.lua
@@ -5,7 +5,7 @@ AddEventHandler("purchaseWeapon", function(name, hash, price, ammo)
     local player = source
     local amount = price
     local character = NDCore.Functions.GetPlayer(source)
-    if characater.bank >= amount then
+    if character.bank >= amount then
         NDCore.Functions.DeductMoney(amount, player, "bank")
         TriggerClientEvent("purchaseWeapon", player, name, hash, price, ammo)   
     end
